6a49bbdf2bfa79c363cd032e0a963c9edf71d6fc
ayiekoderrick-8068 <ayiekoderrick@gmail.com>
2026-04-13 23:50:37 +0300

    feat:addREADME.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # javascript-calculator
+This project entails creation of a calculator that results by use of different functions by the use of array and objects in return it gives results
--- index.js
+++ index.js
@@ -6,48 +6,48 @@
 const finalarray = [];
 let a = 66;
 let b = 4;
-
+//addition
 function addition(a, b) {
     let result = a + b;
     let operator = "+";
     let operands = [a, b];
     return { result, operator, operands };
 }
-
+//subtraction
 function subtraction(a, b) {
     let result = a - b;
     let operator = "-";
     let operands = [a, b];
     return { result, operator, operands };
 }
-
+//multiplication
 function multiplication(a, b) {
     let result = a * b;
     let operator = "*";
     let operands = [a, b];
     return { result, operator, operands };
 }
-
+//division
 function division(a, b) {
     let result = a / b;
     let operator = "/";
     let operands = [a, b];
     return { result, operator, operands };
 }
-
+//function to push the result to the array
 function pushToArray(result) {
     finalarray.push(result);
 }
-
+//performing the operations and pushing the results to the array
 let resulta = addition(a, b);
 pushToArray(resulta);
-
+//performing the operations and pushing the results to the array
 let results = subtraction(a, b);
 pushToArray(results);
-
+//performing the operations and pushing the results to the array
 let resultm = multiplication(a, b);
 pushToArray(resultm);
-
+//performing the operations and pushing the results to the array
 let resultd = division(a, b);
 pushToArray(resultd);
 
